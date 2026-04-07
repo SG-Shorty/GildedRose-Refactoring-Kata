fc0557324f0a9a6e0180fb0ac54b0d983273f2cd
pscheibert <patrick.scheibert@outlook.com>
2026-04-07 16:52:25 +0200

    lost all my commit history because of a spontaneous shutdown. This is my progress so far

--- Java-Approvals/src/main/java/com/gildedrose/Item.java
+++ Java-Approvals/src/main/java/com/gildedrose/Item.java
@@ -2,10 +2,14 @@ package com.gildedrose;
 
 public class Item {
 
+    public static final int MAX_QUALITY = 50;
+    public static final int MIN_QUALITY = 0;
+    public static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
+    public static final String AGED_BRIE = "Aged Brie";
+    public static final String BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT = "Backstage passes to a TAFKAL80ETC concert";
+
     public String name;
-
     public int sellIn;
-
     public int quality;
 
     public Item(String name, int sellIn, int quality) {
@@ -14,8 +18,79 @@ public class Item {
         this.quality = quality;
     }
 
-   @Override
+    @Override
    public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
+    }
+
+    void update() {
+        switch (name) {
+            case AGED_BRIE -> updateAgedBrie();
+            case BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT -> updateBackstagePasses();
+            case SULFURAS_HAND_OF_RAGNAROS -> updateSulfuras();
+            default -> updateDefault();
+        }
+    }
+
+    void updateAgedBrie() {
+        increaseQuality();
+
+        decreaseSellIn();
+
+        if (isExpired()) {
+            increaseQuality();
+        }
+    }
+
+    void updateBackstagePasses() {
+        increaseQuality();
+
+        if (sellIn < 11) {
+            increaseQuality();
+        }
+
+        if (sellIn < 6) {
+            increaseQuality();
+        }
+
+        decreaseSellIn();
+
+        if (isExpired()) {
+            invalidateItem();
+        }
+    }
+
+    void updateDefault() {
+        decreaseQuality();
+
+        decreaseSellIn();
+
+        if (isExpired()) {
+            decreaseQuality();
+        }
+    }
+
+    public void updateSulfuras() {
+        //do nothing
+    }
+
+    void decreaseSellIn() {
+        sellIn = sellIn - 1;
+    }
+
+    void increaseQuality() {
+        quality = Math.min(quality + 1, MAX_QUALITY);
+    }
+
+    void decreaseQuality() {
+        quality = Math.max(quality - 1, MIN_QUALITY);
+    }
+
+    void invalidateItem() {
+        quality = 0;
+    }
+
+    boolean isExpired() {
+        return sellIn < 0;
     }
 }
--- Java-Approvals/src/main/java/com/gildedrose/Program.java
+++ Java-Approvals/src/main/java/com/gildedrose/Program.java
@@ -5,10 +5,10 @@ public class Program {
 	public static void main(String... args) {
 		System.out.println("OMGHAI!");
 
-		Item[] items = new Item[] { 
-				new Item("+5 Dexterity Vest", 10, 20), 
+		Item[] items = new Item[] {
+				new Item("+5 Dexterity Vest", 10, 20),
 				new Item("Aged Brie", 2, 0),
-				new Item("Elixir of the Mongoose", 5, 7), 
+				new Item("Elixir of the Mongoose", 5, 7),
 				new Item("Sulfuras, Hand of Ragnaros", 0, 80),
 				new Item("Sulfuras, Hand of Ragnaros", -1, 80),
 				new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
@@ -22,9 +22,9 @@ public class Program {
 		for (int i = 0; i < 31; i++) {
 			System.out.println("-------- day " + i + " --------");
 			System.out.println("name, sellIn, quality");
-			for (int j = 0; j < items.length; j++) {
-				System.out.println(items[j]);
-			}
+            for (Item item : items) {
+                System.out.println(item);
+            }
 			System.out.println("");
 			app.updateQuality();
 		}
