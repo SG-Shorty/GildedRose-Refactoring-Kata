68130b4226c1d8016b9c51a55563fdb71b612c1c
pscheibert <patrick.scheibert@outlook.com>
2026-04-09 13:53:41 +0200

    remove unused import

--- Java-Approvals/src/test/java/com/gildedrose/ItemTypeTest.java
+++ Java-Approvals/src/test/java/com/gildedrose/ItemTypeTest.java
@@ -6,7 +6,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class ItemTypeTest {
