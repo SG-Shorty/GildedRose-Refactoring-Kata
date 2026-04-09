2561f7291c8e1528e925a960d7981b3c743ce8b8
pscheibert <patrick.scheibert@outlook.com>
2026-04-09 12:45:57 +0200

    extract blocks into methods

--- Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
@@ -1,6 +1,9 @@
 package com.gildedrose;
 
 class GildedRose {
+    public static final String AGED_BRIE = "Aged Brie";
+    public static final String BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT = "Backstage passes to a TAFKAL80ETC concert";
+    public static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -9,61 +12,68 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-
             switch (item.name) {
-                case "Aged Brie" -> {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
+                case AGED_BRIE -> updateAgedBrie(item);
+                case BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT -> updateBackstagePasses(item);
+                case SULFURAS_HAND_OF_RAGNAROS -> updateSulfuras();
+                default -> updateDefault(item);
+            }
+        }
+    }
 
-                    }
+    private void updateAgedBrie(Item item) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
 
-                    item.sellIn = item.sellIn - 1;
+        }
 
-                    if (item.sellIn < 0) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
+        item.sellIn = item.sellIn - 1;
+
+        if (item.sellIn < 0) {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
+            }
+        }
+    }
+
+    private void updateBackstagePasses(Item item) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
+
+            if (item.sellIn < 11) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
                 }
-                case "Backstage passes to a TAFKAL80ETC concert" -> {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
+            }
 
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                    }
-
-                    item.sellIn = item.sellIn - 1;
-
-                    if (item.sellIn < 0) {
-                        item.quality = 0;
-                    }
+            if (item.sellIn < 6) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
                 }
-                case "Sulfuras, Hand of Ragnaros" -> {
-                    //do nothing
-                }
-                default -> {
-                    if (item.quality > 0) {
-                        item.quality = item.quality - 1;
-                    }
+            }
+        }
 
-                    item.sellIn = item.sellIn - 1;
+        item.sellIn = item.sellIn - 1;
 
-                    if (item.sellIn < 0) {
-                        if (item.quality > 0) {
-                            item.quality = item.quality - 1;
-                        }
-                    }
-                }
+        if (item.sellIn < 0) {
+            item.quality = 0;
+        }
+    }
+
+    private void updateSulfuras() {
+        //do nothing
+    }
+
+    private void updateDefault(Item item) {
+        if (item.quality > 0) {
+            item.quality = item.quality - 1;
+        }
+
+        item.sellIn = item.sellIn - 1;
+
+        if (item.sellIn < 0) {
+            if (item.quality > 0) {
+                item.quality = item.quality - 1;
             }
         }
     }
